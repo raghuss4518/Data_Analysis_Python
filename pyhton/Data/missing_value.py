a2e3cb5520bd8ab4600551f448f4e98b3b50d0ff
raghuss4518 <raghuss4518@gmail.com>
2025-01-26 11:24:46 +0530

    to find missing values

--- pyhton/Data/missing_value.py
+++ pyhton/Data/missing_value.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import numpy as np
 
+#Read A Data
 data = pd.read_excel('D:/my-project/Data analysis/Data_Analysis/All-Serices/Python/Resources/Resources/customer_data.xlsx')
-print(data)
+#print(data)
+
+# Missing values
+missing_data = data.isnull().sum()
+print(missing_data)
