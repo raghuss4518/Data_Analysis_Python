dd59b3c39afd17e572e969af9c11425cc83f5075
raghuss4518 <raghuss4518@gmail.com>
2025-01-28 17:00:42 +0530

    fillter a gender data ascending

--- python/DataManipulation/Sort_value.py
+++ python/DataManipulation/Sort_value.py
@@ -5,9 +5,18 @@ import numpy as np
 data = pd.read_excel('D:/my-project/Data analysis/Data_Analysis/All-Serices/Python/Resources/Resources/customer_data.xlsx')
 #print(data)
 
-#sort a value
+#sort a value ascending=False
 sort_value = data.sort_values(by ='CustomerID', ascending=False)
 print(sort_value)
 
+#print sort a value ascending=True
 sort_value1 = data.sort_values(by='CustomerID', ascending=True)
 print(sort_value1)
+
+#sort a value Gender ascending=True
+sort_gender = data.sort_values(by='Gender', ascending=True)
+print(sort_gender)
+
+#print sort a value ascending=False
+sort_value2 = data.sort_values(by='Gender', ascending=False)
+print(sort_value2)
